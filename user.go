487ff0d5aabc17df1b46a1efb02c612cf73c70ca
Brandon Romano <BrandonRRomano@gmail.com>
2024-11-19 09:18:25 -0800

    Revert "Set CreateOrganizationsLimit as an int32 (#357)" (#358)
    This reverts commit 3e10a00a5c90436c75955488acd86eabde62997b.

--- user.go
+++ user.go
@@ -35,7 +35,7 @@ type User struct {
 	VerificationAttemptsRemaining *int64             `json:"verification_attempts_remaining"`
 	DeleteSelfEnabled             bool               `json:"delete_self_enabled"`
 	CreateOrganizationEnabled     bool               `json:"create_organization_enabled"`
-	CreateOrganizationsLimit      *int32             `json:"create_organizations_limit,omitempty"`
+	CreateOrganizationsLimit      *int               `json:"create_organizations_limit,omitempty"`
 	LastActiveAt                  *int64             `json:"last_active_at"`
 	LegalAcceptedAt               *int64             `json:"legal_accepted_at"`
 	CreatedAt                     int64              `json:"created_at"`
