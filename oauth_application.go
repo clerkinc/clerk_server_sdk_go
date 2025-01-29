a3b399c94737b131092ad2a872c7d4f40a1a889a
Haris Chaniotakis <haris@clerk.dev>
2025-01-29 14:36:36 +0200

    feat: Support the OAuth application 'redirect_uris' property
    We enhance the OAuth application response with the new property 'redirect_uris' that will replace
    the existing 'callback_url', which is now marked as deprecated. Also we support passing the new
    property during the create and update operations

--- oauth_application.go
+++ oauth_application.go
@@ -2,22 +2,25 @@ package clerk
 
 type OAuthApplication struct {
 	APIResource
-	Object                string  `json:"object"`
-	ID                    string  `json:"id"`
-	InstanceID            string  `json:"instance_id"`
-	Name                  string  `json:"name"`
-	ClientID              string  `json:"client_id"`
-	ClientSecret          *string `json:"client_secret,omitempty"`
-	Public                bool    `json:"public"`
-	Scopes                string  `json:"scopes"`
-	CallbackURL           string  `json:"callback_url"`
-	DiscoveryURL          string  `json:"discovery_url"`
-	AuthorizeURL          string  `json:"authorize_url"`
-	TokenFetchURL         string  `json:"token_fetch_url"`
-	UserInfoURL           string  `json:"user_info_url"`
-	TokenIntrospectionURL string  `json:"token_introspection_url"`
-	CreatedAt             int64   `json:"created_at"`
-	UpdatedAt             int64   `json:"updated_at"`
+	Object                string   `json:"object"`
+	ID                    string   `json:"id"`
+	InstanceID            string   `json:"instance_id"`
+	Name                  string   `json:"name"`
+	ClientID              string   `json:"client_id"`
+	ClientSecret          *string  `json:"client_secret,omitempty"`
+	Public                bool     `json:"public"`
+	Scopes                string   `json:"scopes"`
+	RedirectURIs          []string `json:"redirect_uris"`
+	DiscoveryURL          string   `json:"discovery_url"`
+	AuthorizeURL          string   `json:"authorize_url"`
+	TokenFetchURL         string   `json:"token_fetch_url"`
+	UserInfoURL           string   `json:"user_info_url"`
+	TokenIntrospectionURL string   `json:"token_introspection_url"`
+	CreatedAt             int64    `json:"created_at"`
+	UpdatedAt             int64    `json:"updated_at"`
+
+	// Deprecated: Use RedirectURIs instead
+	CallbackURL string `json:"callback_url"`
 }
 
 type OAuthApplicationList struct {
